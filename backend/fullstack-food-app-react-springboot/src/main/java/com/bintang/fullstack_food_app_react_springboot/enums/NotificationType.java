666345bf8d2dc079fa36d8f3a911871a370c352b
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-08-14 11:25:46 +0700

    add enum

---
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/enums/NotificationType.java
@@ -0,0 +1,7 @@
+package com.bintang.fullstack_food_app_react_springboot.enums;
+
+public enum NotificationType {
+    EMAIL,
+    SMS,
+    WHATSAPP
+}
---
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/enums/PaymentStatus.java
@@ -0,0 +1,9 @@
+package com.bintang.fullstack_food_app_react_springboot.enums;
+
+public enum PaymentStatus {
+    PENDING,
+    PROCESSING,
+    COMPLETED,
+    FAILED,
+    REFUNDED
+}
