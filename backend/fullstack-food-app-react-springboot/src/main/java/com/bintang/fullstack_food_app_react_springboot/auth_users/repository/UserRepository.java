11fa1b87eb5a8bc0133ae551dbfa004ecffdf32c
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-08-19 06:34:45 +0700

    add user repository

---
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/auth_users/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.bintang.fullstack_food_app_react_springboot.auth_users.repository;
+
+import com.bintang.fullstack_food_app_react_springboot.auth_users.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+
+}
