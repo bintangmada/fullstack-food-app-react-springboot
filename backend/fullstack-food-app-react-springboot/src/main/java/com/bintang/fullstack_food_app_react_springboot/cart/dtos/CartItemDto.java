ac8e8e9aa6ac5b1940f236853e2b3b2883f7ce49
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-08-21 08:45:18 +0700

    add cart item dto

---
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/cart/dtos/CartItemDto.java
@@ -0,0 +1,68 @@
+package com.bintang.fullstack_food_app_react_springboot.cart.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.math.BigDecimal;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class CartItemDto {
+
+    private Long id;
+    private MenuDto menuDto;
+    private int quantity;
+    private BigDecimal pricePerUnit;
+    private BigDecimal subTotal;
+
+    public CartItemDto(Long id, MenuDto menuDto, int quantity, BigDecimal pricePerUnit, BigDecimal subTotal) {
+        this.id = id;
+        this.menuDto = menuDto;
+        this.quantity = quantity;
+        this.pricePerUnit = pricePerUnit;
+        this.subTotal = subTotal;
+    }
+
+    public CartItemDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public MenuDto getMenuDto() {
+        return menuDto;
+    }
+
+    public void setMenuDto(MenuDto menuDto) {
+        this.menuDto = menuDto;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    public void setPricePerUnit(BigDecimal pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public BigDecimal getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(BigDecimal subTotal) {
+        this.subTotal = subTotal;
+    }
+}
