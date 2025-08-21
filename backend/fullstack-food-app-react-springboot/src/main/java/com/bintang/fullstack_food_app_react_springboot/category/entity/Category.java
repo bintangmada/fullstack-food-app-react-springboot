ad0a779cc3645d96e5a6b47f573bdd416e84f356
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-08-21 11:31:43 +0700

    add category entity

---
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/category/entity/Category.java
@@ -0,0 +1,62 @@
+package com.bintang.fullstack_food_app_react_springboot.category.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Menu> menus;
+
+    public Category(Long id, String name, String description, List<Menu> menus) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.menus = menus;
+    }
+
+    public Category() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
+    }
+
+    public void setMenus(List<Menu> menus) {
+        this.menus = menus;
+    }
+}
