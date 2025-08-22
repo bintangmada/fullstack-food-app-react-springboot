eacb875c91864374e197957fb4de9439719958b6
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-08-22 13:58:46 +0700

    update notification entity

--- backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/email_notification/entity/Notification.java
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/email_notification/entity/Notification.java
@@ -28,4 +28,59 @@ public class Notification {
     private final LocalDateTime createdAt = LocalDateTime.now();
 
     private boolean isHtml;
+
+    public Notification() {
+    }
+
+    public Notification(Long id, String recipient, String body, NotificationType type, boolean isHtml) {
+        this.id = id;
+        this.recipient = recipient;
+        this.body = body;
+        this.type = type;
+        this.isHtml = isHtml;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public NotificationType getType() {
+        return type;
+    }
+
+    public void setType(NotificationType type) {
+        this.type = type;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public boolean isHtml() {
+        return isHtml;
+    }
+
+    public void setHtml(boolean html) {
+        isHtml = html;
+    }
 }
