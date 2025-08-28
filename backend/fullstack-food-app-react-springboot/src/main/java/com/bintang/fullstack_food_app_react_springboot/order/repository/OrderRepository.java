63be0643111446968385dac05f6cab96b761f4c8
bintangmada <bintang.mada.suharsono@bni.co.id>
2025-08-29 00:00:36 +0700

    order repository

--- backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/order/repository/OrderRepository.java
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/order/repository/OrderRepository.java
@@ -6,6 +6,7 @@ import com.bintang.fullstack_food_app_react_springboot.order.entity.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -15,5 +16,6 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     List<Order> findByUserOrderByOrderDateDesc(User user);
 
-    
+    @Query("SELECT COUNT(DISTINCT o.user.id) FROM Order o")
+    long countDistinctUsers();
 }
