1a333d10484a32ecad315782c3642bd34b44ef83
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-09-03 09:55:03 +0700

    add role dto

---
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/role/dtos/RoleDto.java
@@ -0,0 +1,36 @@
+package com.bintang.fullstack_food_app_react_springboot.role.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RoleDto {
+
+    private Long id;
+    private String name;
+
+    public RoleDto() {
+    }
+
+    public RoleDto(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
