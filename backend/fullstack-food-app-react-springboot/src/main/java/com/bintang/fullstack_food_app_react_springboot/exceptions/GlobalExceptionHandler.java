861743b42694ecdbffc953c27b45bb2df6f7628b
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-09-06 09:03:26 +0700

    add global exception handler

---
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.bintang.fullstack_food_app_react_springboot.exceptions;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    
+}
