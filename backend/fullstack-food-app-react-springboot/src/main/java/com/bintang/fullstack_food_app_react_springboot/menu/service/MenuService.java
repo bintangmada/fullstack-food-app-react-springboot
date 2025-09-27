5b1e07ee22b9392d70f2ca9b8db04cbaf11e4004
bintangmada <bintang.mada.suharsono@bni.co.id>
2025-09-27 23:23:07 +0700

    menu service

---
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/menu/service/MenuService.java
@@ -0,0 +1,16 @@
+package com.bintang.fullstack_food_app_react_springboot.menu.service;
+
+import com.bintang.fullstack_food_app_react_springboot.menu.dtos.MenuDto;
+import com.bintang.fullstack_food_app_react_springboot.menu.entity.Menu;
+import com.bintang.fullstack_food_app_react_springboot.response.Response;
+
+import java.util.List;
+
+public interface MenuService {
+
+    Response<MenuDto> addMenu(MenuDto menuDto);
+    Response<MenuDto> updateMenu(MenuDto menuDto);
+    Response<MenuDto> getMenuById(Long menuId);
+    Response<?> deleteMenu(Long menuId);
+    Response<List<MenuDto>> getMenus(Long categoryId, String search);
+}
