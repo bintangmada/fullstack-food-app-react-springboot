99b82ab3690705934facdda281e3bb9b2530191c
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-09-29 09:02:12 +0700

    update menu entity, add menu

--- backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/menu/entity/Menu.java
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/menu/entity/Menu.java
@@ -4,12 +4,18 @@ import com.bintang.fullstack_food_app_react_springboot.category.entity.Category;
 import com.bintang.fullstack_food_app_react_springboot.order.entity.OrderItem;
 import com.bintang.fullstack_food_app_react_springboot.review.entity.Review;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
 @Table(name = "menus")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Menu {
 
     @Id
@@ -31,81 +37,4 @@ public class Menu {
     @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL)
     private List<Review> reviews;
 
-    public Menu() {
-    }
-
-    public Menu(Long id, String name, String description, BigDecimal price, String imageUrl, Category category, List<OrderItem> orderItems, List<Review> reviews) {
-        this.id = id;
-        this.name = name;
-        this.description = description;
-        this.price = price;
-        this.imageUrl = imageUrl;
-        this.category = category;
-        this.orderItems = orderItems;
-        this.reviews = reviews;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    public List<OrderItem> getOrderItems() {
-        return orderItems;
-    }
-
-    public void setOrderItems(List<OrderItem> orderItems) {
-        this.orderItems = orderItems;
-    }
-
-    public List<Review> getReviews() {
-        return reviews;
-    }
-
-    public void setReviews(List<Review> reviews) {
-        this.reviews = reviews;
-    }
 }
--- backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/menu/service/MenuServiceImpl.java
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/menu/service/MenuServiceImpl.java
@@ -1,39 +1,91 @@
 package com.bintang.fullstack_food_app_react_springboot.menu.service;
 
+import com.bintang.fullstack_food_app_react_springboot.aws.AwsS3Service;
+import com.bintang.fullstack_food_app_react_springboot.category.entity.Category;
+import com.bintang.fullstack_food_app_react_springboot.category.repository.CategoryRepository;
+import com.bintang.fullstack_food_app_react_springboot.exceptions.BadRequestException;
+import com.bintang.fullstack_food_app_react_springboot.exceptions.NotFoundException;
 import com.bintang.fullstack_food_app_react_springboot.menu.dtos.MenuDto;
+import com.bintang.fullstack_food_app_react_springboot.menu.entity.Menu;
+import com.bintang.fullstack_food_app_react_springboot.menu.repository.MenuRepository;
 import com.bintang.fullstack_food_app_react_springboot.response.Response;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.net.URL;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class MenuServiceImpl implements MenuService{
+
+    private final MenuRepository menuRepository;
+    private final CategoryRepository categoryRepository;
+    private final ModelMapper modelMapper;
+    private final AwsS3Service awsS3Service;
     @Override
     public Response<MenuDto> addMenu(MenuDto menuDto) {
-        return null;
+
+        log.info("Inside addMenu()");
+
+        Category category = categoryRepository.findById(menuDto.getCategoryId())
+                .orElseThrow(() -> new NotFoundException("Category is not found"));
+
+        String imageUrl = null;
+
+        MultipartFile imageFile = menuDto.getImageFile();
+        if(imageFile == null || imageFile.isEmpty()){
+            throw new BadRequestException("Menu image is required");
+        }
+
+        String imageName = UUID.randomUUID() + "_"+imageFile.getOriginalFilename();
+        URL s3Url = awsS3Service.uploadFile("menus/"+imageName, imageFile);
+        imageUrl = s3Url.toString();
+
+        Menu menu = Menu.builder()
+                .name(menuDto.getName())
+                .description(menuDto.getDescription())
+                .price(menuDto.getPrice())
+                .imageUrl(imageUrl)
+                .category(category)
+                .build();
+
+        Menu savedMenu = menuRepository.save(menu);
+
+        return Response.<MenuDto>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Menu created successfully")
+                .data(modelMapper.map(savedMenu, MenuDto.class))
+                .build();
     }
 
     @Override
     public Response<MenuDto> updateMenu(MenuDto menuDto) {
+        log.info("Inside updateMenu()");
         return null;
     }
 
     @Override
     public Response<MenuDto> getMenuById(Long menuId) {
+        log.info("Inside getMenuById()");
         return null;
     }
 
     @Override
     public Response<?> deleteMenu(Long menuId) {
+        log.info("Inside deleteMenu()");
         return null;
     }
 
     @Override
     public Response<List<MenuDto>> getMenus(Long categoryId, String search) {
+        log.info("Inside getMenus()");
         return null;
     }
 }
