56069a12cd1ebf5c9a2362268bbf30df4043f78a
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-10-08 14:24:58 +0700

    cart controller

---
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/cart/controller/CartController.java
@@ -0,0 +1,28 @@
+package com.bintang.fullstack_food_app_react_springboot.cart.controller;
+
+import com.bintang.fullstack_food_app_react_springboot.cart.dtos.CartDto;
+import com.bintang.fullstack_food_app_react_springboot.cart.service.CartService;
+import com.bintang.fullstack_food_app_react_springboot.response.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/cart")
+@RequiredArgsConstructor
+public class CartController {
+
+    private final CartService cartService;
+
+    @PostMapping("/items")
+    public ResponseEntity<Response<?>> addItemToCart(@RequestBody CartDto cartDto){
+        return ResponseEntity.ok(cartService.addItemToCart(cartDto));
+    }
+
+    @PutMapping("/items/increment/{menuId}")
+    public ResponseEntity<Response<?>> incrementItem(@PathVariable("menuId") Long menuId){
+        return ResponseEntity.ok(cartService.incrementItem(menuId));
+    }
+
+    
+}
