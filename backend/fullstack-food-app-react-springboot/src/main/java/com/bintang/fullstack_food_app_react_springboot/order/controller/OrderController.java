22b645f89024a8aee50c2bd571d3c45fbfb11ab5
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-10-19 10:56:21 +0700

    getAllOrders

--- backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/order/controller/OrderController.java
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/order/controller/OrderController.java
@@ -1,10 +1,12 @@
 package com.bintang.fullstack_food_app_react_springboot.order.controller;
 
+import com.bintang.fullstack_food_app_react_springboot.enums.OrderStatus;
 import com.bintang.fullstack_food_app_react_springboot.order.dto.OrderDto;
 import com.bintang.fullstack_food_app_react_springboot.order.dto.OrderItemDto;
 import com.bintang.fullstack_food_app_react_springboot.order.service.OrderService;
 import com.bintang.fullstack_food_app_react_springboot.response.Response;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -20,23 +22,31 @@ public class OrderController {
 
     @PostMapping("/checkout")
     @PreAuthorize("hasAuthority('CUSTOMER')")
-    public ResponseEntity<Response<?>> checkout(){
+    public ResponseEntity<Response<?>> checkout() {
         return ResponseEntity.ok(orderService.placeOrderFromCart());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Response<?>> getOrderById(@PathVariable("id") Long id){
+    public ResponseEntity<Response<?>> getOrderById(@PathVariable("id") Long id) {
         return ResponseEntity.ok(orderService.getOrderById(id));
     }
 
     @GetMapping("/me")
-    public ResponseEntity<Response<List<OrderDto>>> getMyOrders(){
+    public ResponseEntity<Response<List<OrderDto>>> getMyOrders() {
         return ResponseEntity.ok(orderService.getOrdersOfUser());
     }
 
     @GetMapping("order-item/{orderItemId}")
-    public ResponseEntity<Response<OrderItemDto>> getOrderItemById(@PathVariable("orderItemId") Long orderItemId){
+    public ResponseEntity<Response<OrderItemDto>> getOrderItemById(@PathVariable("orderItemId") Long orderItemId) {
         return ResponseEntity.ok(orderService.getOrderItemById(orderItemId));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Response<Page<OrderDto>>> getAllOrders(
+            @RequestParam(required = false) OrderStatus orderStatus,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "100") int size) {
+        return ResponseEntity.ok(orderService.getAllOrders(orderStatus, page, size));
     }
 
 }
