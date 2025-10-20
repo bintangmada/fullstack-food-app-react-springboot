6278130b8f4357fa17dbabd13273510c9e551570
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-10-20 09:28:40 +0700

    Review Service

--- backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/review/service/ReviewService.java
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/review/service/ReviewService.java
@@ -1,7 +1,14 @@
 package com.bintang.fullstack_food_app_react_springboot.review.service;
 
+import com.bintang.fullstack_food_app_react_springboot.response.Response;
+import com.bintang.fullstack_food_app_react_springboot.review.dtos.ReviewDto;
+
+import java.util.List;
+
 public interface ReviewService {
 
+    Response<ReviewDto> createReview(ReviewDto reviewDto);
+    Response<List<ReviewDto>> getReviewsForMenu(Long menuId);
 
 
 }
