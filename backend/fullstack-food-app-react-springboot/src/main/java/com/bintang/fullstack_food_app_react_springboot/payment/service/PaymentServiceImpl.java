234cd4b68427adf97a50a3d767725e702a9b3ff0
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-10-23 10:13:01 +0700

    PaymentServiceImpl

---
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/payment/service/PaymentServiceImpl.java
@@ -0,0 +1,47 @@
+package com.bintang.fullstack_food_app_react_springboot.payment.service;
+
+import com.bintang.fullstack_food_app_react_springboot.email_notification.entity.Notification;
+import com.bintang.fullstack_food_app_react_springboot.email_notification.services.NotificationService;
+import com.bintang.fullstack_food_app_react_springboot.order.repository.OrderRepository;
+import com.bintang.fullstack_food_app_react_springboot.payment.dto.PaymentDto;
+import com.bintang.fullstack_food_app_react_springboot.payment.repository.PaymentRepository;
+import com.bintang.fullstack_food_app_react_springboot.response.Response;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+
+import java.util.List;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class PaymentServiceImpl implements PaymentService{
+
+    private final PaymentRepository paymentRepository;
+    private final NotificationService notificationService;
+    private final TemplateEngine templateEngine;
+    private final OrderRepository orderRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public Response<?> initializePayment(PaymentDto paymentDto) {
+        return null;
+    }
+
+    @Override
+    public void updatePaymentForOrder(PaymentDto paymentDto) {
+
+    }
+
+    @Override
+    public Response<List<PaymentDto>> getAllPayments() {
+        return null;
+    }
+
+    @Override
+    public Response<PaymentDto> getPaymentById(Long paymentId) {
+        return null;
+    }
+}
