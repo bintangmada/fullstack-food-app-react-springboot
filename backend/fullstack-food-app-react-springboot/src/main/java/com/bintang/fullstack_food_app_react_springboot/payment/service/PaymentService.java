412fcbaf26d4ad4213fcae87866b6db0d8b7fd72
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-10-23 09:50:59 +0700

    PaymentService

--- backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/payment/service/PaymentService.java
+++ backend/fullstack-food-app-react-springboot/src/main/java/com/bintang/fullstack_food_app_react_springboot/payment/service/PaymentService.java
@@ -1,2 +1,15 @@
-package com.bintang.fullstack_food_app_react_springboot.payment.service;public interface PaymentService {
+package com.bintang.fullstack_food_app_react_springboot.payment.service;
+
+import com.bintang.fullstack_food_app_react_springboot.payment.dto.PaymentDto;
+import com.bintang.fullstack_food_app_react_springboot.response.Response;
+
+import java.util.List;
+
+public interface PaymentService {
+
+    Response<?> initializePayment(PaymentDto paymentDto);
+    void updatePaymentForOrder(PaymentDto paymentDto);
+    Response<List<PaymentDto>> getAllPayments();
+    Response<PaymentDto> getPaymentById(Long paymentId);
+
 }
