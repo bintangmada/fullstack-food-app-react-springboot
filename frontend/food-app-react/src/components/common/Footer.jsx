85cc74d03ff6d8caddf0187f895670b48c5b8a5f
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-11-10 09:35:57 +0700

    update footer

--- frontend/food-app-react/src/components/common/Footer.jsx
+++ frontend/food-app-react/src/components/common/Footer.jsx
@@ -1,3 +1,22 @@
-const Footer = () => {};
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; {new Date().getFullYear()} Bintang Food App</p>
+        <div className="footer-links">
+          <a href="#" className="footer-link">
+            Term of Service
+          </a>
+          <a href="#" className="footer-link">
+            Privacy Policy
+          </a>
+          <a href="#" className="footer-link">
+            Contact Us
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
